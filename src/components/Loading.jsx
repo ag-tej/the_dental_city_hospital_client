06a81b7e33ceff57de64f9af2472e3eb14350eca
ag-tej <tej.agrawal59@gmail.com>
2025-03-06 21:10:03 +0545

    fix logo width

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,7 +3,7 @@ import logo from "../assets/logo.webp";
 const Loading = () => {
   return (
     <div className="w-screen h-screen flex items-center justify-center animate-pulse p-8">
-      <img src={logo} alt="The Dental City" />
+      <img src={logo} alt="The Dental City" className="w-1/2 max-w-sm" />
     </div>
   );
 };
